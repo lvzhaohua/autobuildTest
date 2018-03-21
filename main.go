cf5c1b8c7f26377ee98156a1b3184ac50ad34949
zhlv <zhlv@bnstar.com>
2018-03-21 15:32:24 +0800

    commit dockerfile

--- main.go
+++ main.go
@@ -7,5 +7,8 @@ func main() {
 	engine.GET("/hello", func(context *gin.Context) {
 		context.String(200,"hello gin ...")
 	})
+	engine.GET("/hello1", func(context *gin.Context) {
+		context.String(200,"hello1 gin ...")
+	})
 	engine.Run(":8200")
 }
