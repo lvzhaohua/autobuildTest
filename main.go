60e8f1841df87b23cc8aacaefb46518df394bc3b
zhlv <zhlv@bnstar.com>
2019-01-24 12:00:33 +0800

    commit

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import "github.com/gin-gonic/gin"
 func main() {
 	engine := gin.New()
 	engine.GET("/hello", func(context *gin.Context) {
-		context.String(200,"hello gin ...")
+		context.String(200, "hello gin ...")
 	})
 	engine.GET("/hello1", func(context *gin.Context) {
-		context.String(200,"hello1 gin ...")
+		context.String(200, "hello1 gin ...")
 	})
 	engine.Run(":8200")
+
 }
